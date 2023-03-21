8f5625e4cd9788de51bd51db44a3bc5cedbea6f4
jaykumarcunomial <jay.kumar@cunomial.com>
2023-03-21 19:03:09 +0530

    added cta section

---
+++ components/home/CTA.js
@@ -0,0 +1,52 @@
+export default function CTA() {
+  return (
+    <div className="relative bg-gray-900">
+      <div className="relative h-80 overflow-hidden bg-indigo-600 md:absolute md:left-0 md:h-full md:w-1/3 lg:w-1/2">
+        <img
+          className="h-full w-full object-cover"
+          src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80"
+          alt=""
+        />
+        <svg
+          viewBox="0 0 926 676"
+          aria-hidden="true"
+          className="absolute left-24 -bottom-24 w-[57.875rem] transform-gpu blur-[118px]"
+        >
+          <path
+            fill="url(#60c3c621-93e0-4a09-a0e6-4c228a0116d8)"
+            fillOpacity=".4"
+            d="m254.325 516.708-90.89 158.331L0 436.427l254.325 80.281 163.691-285.15c1.048 131.759 36.144 345.144 168.149 144.613C751.171 125.508 707.17-93.823 826.603 41.15c95.546 107.978 104.766 294.048 97.432 373.585L685.481 297.694l16.974 360.474-448.13-141.46Z"
+          />
+          <defs>
+            <linearGradient
+              id="60c3c621-93e0-4a09-a0e6-4c228a0116d8"
+              x1="926.392"
+              x2="-109.635"
+              y1=".176"
+              y2="321.024"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#776FFF" />
+              <stop offset={1} stopColor="#FF4694" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
+      <div className="relative mx-auto max-w-7xl py-24 sm:py-32 lg:py-40 lg:px-8">
+        <div className="pr-6 pl-6 md:ml-auto md:w-2/3 md:pl-16 lg:w-1/2 lg:pl-24 lg:pr-0 xl:pl-32">
+          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-5xl">
+            STARSHIP SELECTED BY NASA TO SUPPORT SUSTAINED LUNAR EXPLORATION
+          </h2>
+          <div className="mt-10 flex items-center gap-x-6">
+            <a
+              href="https://www.spacex.com/updates/#sustained-lunar-exploration"
+              className="bg-transparent py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm ring-2 ring-inset ring-white hover:bg-gray-50 hover:text-gray-900"
+            >
+              LEARN MORE
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
